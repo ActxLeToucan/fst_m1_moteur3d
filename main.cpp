d8602309466257fa229f5cfe1dbd9547428a2fc9
Antoine CONTOUX <contoux1u@etu.univ-lorraine.fr>
2024-03-13 11:55:25 +0100

    draw line

--- main.cpp
+++ main.cpp
@@ -17,6 +17,36 @@ struct Triangle {
     int vn1, vn2, vn3;
 };
 
+void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1-x0;
+    int dy = y1-y0;
+    int derror2 = std::abs(dy)*2;
+    int error2 = 0;
+    int y = y0;
+    for (int x=x0; x<=x1; x++) {
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1>y0?1:-1);
+            error2 -= dx*2;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
@@ -62,5 +92,8 @@ int main(int argc, char **argv) {
         int y = (p.y + 1) * 400;
         image.set(x, y, TGAColor{255, 255, 255, 255});
     }
+    drawLine(10, 10, 200, 300, image, TGAColor{255, 0, 0, 255});
+    drawLine(10, 10, 200, 100, image, TGAColor{0, 255, 0, 255});
+    drawLine(10, 10, 100, 200, image, TGAColor{0, 0, 255, 255});
     image.write_tga_file("output.tga");
 }
